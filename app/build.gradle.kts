560ef5568ba1004526095486c2d3acae26e8374b
yhas1984 <ymiy2021@gmail.com>
2026-07-08 18:53:37 +0200

    build: revert to versionCode=2 for v1.0.2
    The versionCode=3 value was already used by v1.0.1 which was
    previously published to Google Play. To avoid a versionCode collision,
    this patch bumps v1.0.2 to versionCode=2 instead, reusing the slot
    that was freed by other cleanup.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         applicationId = "com.gastos.ingresos"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
+        versionCode = 2
         versionName = "1.0.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
