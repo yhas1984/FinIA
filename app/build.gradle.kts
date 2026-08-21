109a787db26db026142f1db459ca46921ad1bded
yhas1984 <ymiy2021@gmail.com>
2026-08-21 19:56:43 +0200

    chore(release): bump a versionCode 16 (1.7.0)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.gastos.ingresos"
         minSdk = 26
         targetSdk = 36
-        versionCode = 15
-        versionName = "1.6.0"
+        versionCode = 16
+        versionName = "1.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
